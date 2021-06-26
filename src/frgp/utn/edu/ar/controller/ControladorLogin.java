243361514ed4544eb1aecd455c5c2129937eec36
Pedr0-Tonell0 <pedrotonello11@gmail.com>
2021-06-26 18:18:08 -0300

    Perfil del cliente
    Cuando se loguea el cliente, le muestra en el home las cuentas que tiene asociadas

--- src/frgp/utn/edu/ar/controller/ControladorLogin.java
+++ src/frgp/utn/edu/ar/controller/ControladorLogin.java
@@ -45,8 +45,12 @@ public class ControladorLogin {
 				{
 					Persona Persona = new Persona();
 					Persona = (Persona) negocioPersona.obtenerPersona(UsuarioLogin.getDni());
+					ArrayList<Cuenta> listaCuenta = (ArrayList<Cuenta>) negocioPersona.obtenerCuenta(UsuarioLogin.getDni());
+					
 					HttpSession misession = request.getSession(true);
 					misession.setAttribute("Usuario",Persona);
+					//misession.setAttribute("listaCuenta", listaCuenta);
+					MV.addObject("listaCuenta", listaCuenta);
 					MV.setViewName("Cliente");
 					return MV;
 				}
