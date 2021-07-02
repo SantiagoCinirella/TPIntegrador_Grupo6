9179e0251a88090e01641d5edb76e4ceccbec4f0
scinirella <cinirellasantiago@gmail.com>
2021-07-01 22:15:51 -0300

    Se agrega interfaz a las clases

--- src/frgp/utn/edu/ar/controller/ControladorCliente.java
+++ src/frgp/utn/edu/ar/controller/ControladorCliente.java
@@ -3,10 +3,6 @@ package frgp.utn.edu.ar.controller;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
