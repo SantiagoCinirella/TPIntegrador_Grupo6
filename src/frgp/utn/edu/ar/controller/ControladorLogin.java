d9c538360586a6f624d47ebd4f3db05fa202919c
scinirella <cinirellasantiago@gmail.com>
2021-07-08 20:21:42 -0300

    Fix ControladorLogin

--- src/frgp/utn/edu/ar/controller/ControladorLogin.java
+++ src/frgp/utn/edu/ar/controller/ControladorLogin.java
@@ -1,5 +1,7 @@
 package frgp.utn.edu.ar.controller;
 
+import java.util.ArrayList;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -10,8 +12,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import frgp.utn.edu.ar.entidad.Cuenta;
 import frgp.utn.edu.ar.entidad.Persona;
 import frgp.utn.edu.ar.negocio.LoginNegocio;
+import frgp.utn.edu.ar.negocio.NegCuenta;
 import frgp.utn.edu.ar.negocio.NegPersona;
 
 @Controller
@@ -20,17 +24,17 @@ public class ControladorLogin {
 	@Autowired
 	@Qualifier("servicioPersona")
 	private NegPersona negocioPersona;
-	
+
 	@Autowired
 	@Qualifier("loginNegocio")
 	private LoginNegocio loginNegocio;
-	
+
 	@Autowired
 	private Persona Persona;
 
 	@Autowired
-	private ControladorCuenta ControladorCuenta;
-	
+	private NegCuenta negocioCuenta;
+
 	@RequestMapping("altaDeUsuarios.html")
 	public ModelAndView eventoRedirectLogin(String txtUsuario, String txtPassword, HttpServletRequest request,
 			HttpServletResponse response) {
@@ -39,21 +43,20 @@ public class ControladorLogin {
 		try {
 			Persona = loginNegocio.verificarUsuario(txtUsuario, txtPassword);
 			if (!(Persona == null)) {
-				if(Persona.getUsuario().getTipoUsuario()== false)
-				{
+				if (Persona.getUsuario().getTipoUsuario() == false) {
 					HttpSession misession = request.getSession(true);
 					misession.setAttribute("Usuario", Persona);
-					ControladorCuenta ControladorCuenta = new ControladorCuenta();
-					return ControladorCuenta.eventoRedireccionarPag1();
+					ArrayList<Cuenta> listaPersona = new ArrayList<>();
+					listaPersona = (ArrayList<Cuenta>) negocioCuenta.listarCuentasBajaLogica();
+					MV.addObject("listaPersona", listaPersona);
+					MV.setViewName("ABMLCuentas");
+					return MV;
+				} else {
+					ArrayList<Cuenta> listaCuenta = (ArrayList<Cuenta>) negocioPersona.obtenerCuenta(Persona.getDni());
+					MV.addObject("listaCuenta", listaCuenta);
+					MV.setViewName("Cliente");
+					return MV;
 				}
-				else
-				{
-					HttpSession misession = request.getSession(true);
-					misession.setAttribute("Usuario",Persona);
-					return ControladorCuenta.eventoRedireccionarPagCliente(Persona.getDni());
-					
-				}
-
 			} else {
 				MV.setViewName("Login");
 			}
